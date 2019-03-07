44601a261b0ff6283e7108a36fd6cd4233d579e9
peng.xing <southeast_px@163.com>
2019-03-07 14:38:19 +0800

    文件附加器！

--- src/main/java/cn/ksdshpx/log4j/Log4jDemo2.java
+++ src/main/java/cn/ksdshpx/log4j/Log4jDemo2.java
@@ -8,12 +8,13 @@ import org.apache.log4j.Logger;
  * Create by peng.x
  * Date: 2019/3/7
  * Time: 11:16
- * Description:
+ * Description:各种日志附加器的演示
  */
 public class Log4jDemo2 {
     public static void main(String[] args) {
         //创建日志记录工具
         Logger logger = LogManager.getLogger(Log4jDemo2.class);
+        logger.trace("trace message");
         logger.debug("debug message");
         logger.info("info message");
         logger.warn("warn message");
