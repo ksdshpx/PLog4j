b800988844772afa73ee11e6f02eb97c993e6472
peng.xing <southeast_px@163.com>
2019-03-07 11:29:48 +0800

    Log4j控制台附加器！

---
+++ src/main/java/cn/ksdshpx/log4j/Log4jDemo2.java
@@ -0,0 +1,23 @@
+package cn.ksdshpx.log4j;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2019/3/7
+ * Time: 11:16
+ * Description:
+ */
+public class Log4jDemo2 {
+    public static void main(String[] args) {
+        //创建日志记录工具
+        Logger logger = LogManager.getLogger(Log4jDemo2.class);
+        logger.debug("debug message");
+        logger.info("info message");
+        logger.warn("warn message");
+        logger.error("error message");
+        logger.fatal("fatal message");
+    }
+}
